c193b01060fe3cdac73594c8369e4860d2113828
Peter Molnar <peter.molnar@falconsocial.com>
2015-11-24 23:22:48 +0100

    Added verification method for rows and columns.

--- src/sudokju/core.clj
+++ src/sudokju/core.clj
@@ -16,6 +16,32 @@
       (json/read-str
         (slurp file)))
 
+(defn has-duplicates
+  "Checks if the passed list contains duplicates except for zeros"
+  [ row ]
+  (>
+    (count
+      (for [[id freq] (frequencies row)
+        :when (and (not (= id 0)) (> freq 1))]
+        id))
+    0)
+  )
+
+(defn verify-row
+  "Checks the specified row of the passed problem, if it's valid."
+  [ problem rowNum ]
+  (has-duplicates (nth problem rowNum)))
+
+(defn verify-column
+  "Checks the specified column of the passed problem if it's valid."
+  [ problem colNum ]
+  (has-duplicates (map nth problem (repeat 9 colNum))))
+
+;(defn verify-segment
+;  "Checks if the specified segment is valid"
+;  [ problem segmentNum ]
+;  (has-duplicates ))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
