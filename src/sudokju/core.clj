2e62d24eeb49720f204285f6d7d4e8c2f3239d03
Peter Molnar <mp@mpsw.hu>
2016-03-28 22:49:08 +0200

    Added function to get rows and columns.

--- src/sudokju/core.clj
+++ src/sudokju/core.clj
@@ -1,14 +1,7 @@
 (ns sudokju.core
-    (:require [clojure.data.json :as json])
+  (:require [clojure.data.json :as json]
+            [clojure.set :as set])
     (:gen-class))
-
-(defn print-problem
-  "Prints a problem"
-  [p]
-  (doseq [ row p]
-    (doseq [ cell row ]
-      (print cell " "))
-    (println)))
 
 (defn read-problem
       "Reads a problem from json"
@@ -16,13 +9,91 @@
       (json/read-str
         (slurp file)))
 
+(defn print-problem
+  "Prints a problem"
+  [ p ]
+  (doseq [ row p ]
+    (doseq [ val row ]
+      (print val " "))
+    (println)))
+
+(defn base-n
+  "Returns the base number of the problem ie number of rows and columns"
+  [ problem ]
+  (+ (count problem) 1))
+
+(defn base-k
+  "Returns the square root of the base number."
+  [ problem ]
+  (sqrt (base-n problem)))
+
+(defn row
+  "Returns the i-th row as a list"
+  [ problem i ]
+  (nth problem i))
+
+(defn col
+  "Returns the j-th column as a list"
+  [ problem j ]
+  (map nth problem (repeat (base-n problem) j)))
+
+(defn cell
+  "Returns the k x k cell which the (i,j) position belongs to"
+  [ problem i j ]
+  (let [ k (base-k problem)
+         n (base-n problem) ]
+    (let [ i-start (* k (quot i k))
+           i-end (* (+ k 1) (quot i k))
+           j-start (* k (quot j k))
+           j-end (* (+ k 1) (quot j k))]
+
+    )
+
+
+
+  (loop [ p ])
+  )
+
+(defn candidates-base
+  "Returns the basic set of candidates."
+  [problem]
+  (set (range 1 (+ (count problem) 1))))
+
+(defn candidates-row
+  "Finds candidates for the specified row, i-th row"
+  [problem i]
+  (set/difference
+    (candidates-base problem)
+    (set (row problem i)))
+  )
+
+(defn candidates-column
+  "Finds candidates in the specified column"
+  [ problem j ]
+  (set/difference
+    (candidates-base problem)
+    (set (col problem j)))
+  )
+
+;(defn candidates
+;  "Finds candidates for the specified space, i-th row, j-th column"
+;  [problem i j]
+;  (set/intersection
+;    (candidates-row problem i)
+;    (candidates-column problem j)
+;    (candidates-square problem i j))
+;  )
+
 (defn has-duplicates
   "Checks if the passed list contains duplicates except for zeros"
   [ row ]
   (>
     (count
-      (for [[id freq] (frequencies row)
-        :when (and (not (= id 0)) (> freq 1))]
+      (for [
+            [id freq]
+            (frequencies row)
+            :when (and (not (= id 0)) (> freq 1))
+            ]
         id))
     0)
   )
@@ -35,12 +106,13 @@
 (defn verify-column
   "Checks the specified column of the passed problem if it's valid."
   [ problem colNum ]
-  (has-duplicates (map nth problem (repeat 9 colNum))))
+  (has-duplicates ))
 
-;(defn verify-segment
-;  "Checks if the specified segment is valid"
-;  [ problem segmentNum ]
-;  (has-duplicates ))
+  ;(defn verify-segment
+  ;  "Checks if the specified segment is valid"
+  ;  [ problem segmentNum ]
+  ;  (let i range (quot (count problem segmentNum) (+ (count problem segmentNum) 2))))
+
 
 (defn -main
   "I don't do a whole lot ... yet."
