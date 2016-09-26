1c8fe9caf27b1887fa855053b33e350615cb3038
Peter Molnar <peter.molnar@falconsocial.com>
2016-09-26 20:18:01 +0200

    Added done and correct tests.

--- src/sudokju/solver.clj
+++ src/sudokju/solver.clj
@@ -8,6 +8,38 @@
   (for [i (range 0 (:base puzzle))
         j (range 0 (:base puzzle))
         n (candidates puzzle i j)]
-      {:i i :j j :value n}
-    ))
+      {:i i :j j :value n :weight (count (candidates puzzle i j))}))
 
+(defn move
+  "Perform move on puzzle"
+  [puzzle-with-history move]
+   {:puzzle (fill-pos (:puzzle puzzle-with-history)  (:i move) (:j move) (:value move))
+    :history (cons move (:history puzzle-with-history))})
+
+(defn done?
+  "Tests if a puzzle is done"
+  [puzzle-with-history]
+  (let [{:keys [puzzle history]} puzzle-with-history]
+    (every?
+      (partial not-any? (partial = 0))
+      (:matrix puzzle))))
+
+(defn correct?
+  "Tests if a puzzle is correct"
+  [ puzzle-with-history ]
+  (let [puzzle (:puzzle puzzle-with-history)
+        base (:base puzzle)
+        root (:root puzzle)
+        complete? (partial = (set (range 1 (inc base))))]
+    (and
+      (every? complete? (map set (:matrix puzzle)))
+      (every? complete?
+              (for [ j (range 0 base)]
+                (set (col puzzle j))))
+      (every? complete?
+              (for [i (range 0 base root)
+                    j (range 0 base root)]
+                (set (cell puzzle i j))))
+      )
+    )
+  )
--- test/sudokju/solver_test.clj
+++ test/sudokju/solver_test.clj
@@ -4,18 +4,40 @@
             [sudokju.solver :refer :all]))
 
 (def puzzle {:base 4 :root 2 :matrix [[3 0 4 0] [0 1 0 3] [2 3 0 0] [1 0 0 2]] })
+(def done-puzzle {:base 4 :root 2 :matrix [[3 2 4 1] [4 1 2 3] [2 3 1 4] [1 4 3 2]] })
+(def incorrent-puzzle {:base 4 :root 2 :matrix [[3 2 4 4] [4 1 2 3] [2 3 1 4] [1 4 3 2]] })
 
 (deftest test-next-moves
   (testing "list next moves"
     ;(pprint (next-moves puzzle))
-    (is (= [{:i 0 :j 1 :value 2}
-            {:i 0 :j 3 :value 1}
-            {:i 1 :j 0 :value 4}
-            {:i 1 :j 2 :value 2}
-            {:i 2 :j 2 :value 1}
-            {:i 2 :j 3 :value 1}
-            {:i 2 :j 3 :value 4}
-            {:i 3 :j 1 :value 4}
-            {:i 3 :j 2 :value 3}
-            ]
+    (is (= [{:i 0 :j 1 :value 2 :weight 1}
+            {:i 0 :j 3 :value 1 :weight 1}
+            {:i 1 :j 0 :value 4 :weight 1}
+            {:i 1 :j 2 :value 2 :weight 1}
+            {:i 2 :j 2 :value 1 :weight 1}
+            {:i 2 :j 3 :value 1 :weight 2}
+            {:i 2 :j 3 :value 4 :weight 2}
+            {:i 3 :j 1 :value 4 :weight 1}
+            {:i 3 :j 2 :value 3 :weight 1}]
            (next-moves puzzle)))))
+
+(deftest test-move
+  (testing "make move"
+    (is (= {:puzzle {:base 4 :root 2 :matrix [[3 2 4 0] [0 1 0 3] [2 3 0 0] [1 0 0 2]] }
+            :history '({:i 0 :j 1 :value 2 :weight 1})}
+           (move {:puzzle puzzle :history ()} {:i 0 :j 1 :value 2 :weight 1})))))
+
+(deftest test-done
+  (testing "test not done"
+    (is (not (done? {:puzzle puzzle :history ()}))))
+  (testing "done"
+    (is (done? {:puzzle done-puzzle :history ()}))))
+
+(deftest test-correct
+  (testing "test correct"
+    (is (correct? {:puzzle done-puzzle :history ()})))
+  (testing "not correct because unfinished"
+    (is (not (correct? {:puzzle puzzle :history ()}))))
+  (testing "incorrect"
+    (is (not (correct? {:puzzle incorrent-puzzle :history ()})))))
+
